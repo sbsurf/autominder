13f7999967b7b746986aa8254de09945bc2b04c4
yuri <yuri@nanakdev.com>
2015-03-10 08:06:39 -0700

    Removing manufacturer migration, model.

--- db/migrate/20150310142125_create_manufacturers.rb
+++
@@ -1,9 +0,0 @@
-class CreateManufacturers < ActiveRecord::Migration
-  def change
-    create_table :manufacturers do |t|
-      t.string :name
-
-      t.timestamps null: false
-    end
-  end
-end
--- spec/models/manufacturer_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Manufacturer, type: :model do
-  it { is_expected.to respond_to(:name) }
-end
