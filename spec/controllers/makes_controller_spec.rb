d33a6eeff2041e46d83a562b22237f55f49c0e55
yuri <yuri@nanakdev.com>
2015-03-11 13:09:36 -0700

    Mocking calls to Edmunds gem in makes controller spec.

--- spec/controllers/makes_controller_spec.rb
+++ spec/controllers/makes_controller_spec.rb
@@ -2,22 +2,38 @@ require 'rails_helper'
 
 RSpec.describe MakesController, type: :controller do
   describe "GET #index" do
+    let(:edmunds) { instance_double('Edmunds::Make') }
+
+    before do
+      allow(Edmunds::Make).to receive(:new).and_return(edmunds)
+    end
+
     it "returns http success" do
+      allow(edmunds).to receive(:find_all).and_return([])
       get :index
       expect(response).to have_http_status(:success)
     end
 
     describe "returns array of makes" do
       it "filters by year" do
+        expect(edmunds).to receive(:find_makes_by_model_year).with('2015').and_return([{id: 200002038, models: []}, {id: 200464140, models: []}])
+        # TODO: replace with factories
+
         get :index, year: '2015'
+
         json = JSON.parse(response.body)
-        expect(json['makes'].length).to eq(42)
+        expect(json['makes']).to eq([{"id" => 200002038, "models" => []}, {"id" => 200464140, "models" => []}])
       end
 
       it "returns all" do
+        # allow(Edmunds::Make).to receive(:new).and_return(edmunds)
+        expect(edmunds).to receive(:find_all).and_return([{id: 200002038, models: []}, {id: 200464140, models: []}, {id: 200000001, models: []}])
+        # TODO: replace with factories
+
         get :index
+
         json = JSON.parse(response.body)
-        expect(json['makes'].length).to eq(61)
+        expect(json['makes']).to eq([{"id" => 200002038, "models" => []}, {"id" => 200464140, "models" => []}, {"id" => 200000001, "models" => []}])
       end
     end
   end
